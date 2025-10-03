315d5258d3bc82325928e4a467ad638c285867f3
jav_bankdata <jav@bankdata.dk>
2025-10-03 23:32:41 +0200

    Playing around with vars and enums

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -1,21 +1,34 @@
-//
-//  ContentView.swift
-//  YouAreAwesome
-//
-//  Created by Jakob Skov Søndergård on 03/10/2025.
-//
+    //
+    //  ContentView.swift
+    //  YouAreAwesome
+    //
+    //  Created by Jakob Skov Søndergård on 03/10/2025.
+    //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var selectedMessage: MessageIndex = MessageIndex.one
+
+    enum MessageIndex: Int, CaseIterable {
+        case one, two, three
+
+        func next() -> MessageIndex {
+            guard let currentIndex = Self.allCases.firstIndex(of: self) else {
+                return .one
+            }
+            let nextIndex = (currentIndex + 1) % Self.allCases.count
+            return Self.allCases[nextIndex]
+        }
+    }
+    let messages = ["Hello, World!", "You are awesome!", "Check it out!"]
     var body: some View {
-        
+
         VStack {
             Text("YOU ARE AWESOME!")
                 .font(.largeTitle)
                 .fontWeight(.thin)
                 .foregroundStyle(.blue)
-            
             HStack {
                 Image(systemName: "swiftdata")
                     .resizable()
@@ -29,6 +42,10 @@ struct ContentView: View {
                     .resizable()
                     .scaledToFit()
                     .foregroundStyle(.tint)
+            }
+            Spacer()
+            Button(messages[selectedMessage.rawValue]){
+                selectedMessage = selectedMessage.next()
             }
         }
         .padding()
